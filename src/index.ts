8dedac5cec7d786a56a5f918e96305a4e9f4734c
developerquartz <developerquartz@gmail.com>
2026-04-15 18:06:32 +0530

    allowed from all ips

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ dotenv.config();
 
 const PORT: number = Number(process.env.PORT) || 5000;
 
-const server = app.listen(PORT, () => {
+const server = app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
 
